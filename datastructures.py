2d29247ccacb49b38fb541d968477f2a452b7b78
Pepijn <pbakker@nikhef.nl>
2016-12-20 17:16:20 +0100

    This -> self in datastructures

--- datastructures.py
+++ datastructures.py
@@ -3,25 +3,25 @@ from globals import *
 
 class Hit:
     def __init__(self,pulselist_):
-        this.pulses = pulselist_
-        this.x = 0.
-        this.y = 0.
-        this.time = 0.
-        this.chiSquared = 0.
+        self.pulses = pulselist_
+        self.x = 0.
+        self.y = 0.
+        self.time = 0.
+        self.chiSquared = 0.
 
     def setCoords(self,x_,y_,time_,chiSquared_):
-        this.x = x_
-        this.y = y_
-        this.time = time_
-        this.chiSquared = chiSquared_
+        self.x = x_
+        self.y = y_
+        self.time = time_
+        self.chiSquared = chiSquared_
 
 class Pulse:
     def __init__(self,id_,time_):
-        this.id = id_
-        this.time = time_
+        self.id = id_
+        self.time = time_
 
 class Light:
     def __init__(self,x_,y_,time_):
-        this.x = x_
-        this.y = y_
-        this.time = time_
+        self.x = x_
+        self.y = y_
+        self.time = time_
